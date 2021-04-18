9fb3406bc4db28c615ac3f01736599d458a4ab4a
Marcus Smolenski <marcussmolenski00@gmail.com>
2021-04-18 14:03:16 -0400

    Simplified colorArray

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let colorArray = [["red", 0], ["green", 1], ["blue", 2]];
+let colorArray = ["red", "green", "blue"];
 
 let count = 0;
 
 function domManipulation() {
 	let initialHeader = document.getElementById('initial-header');
 
-	initialHeader.style.color = colorArray[count][0];
+	initialHeader.style.color = colorArray[count];
 
 	++count;
 
